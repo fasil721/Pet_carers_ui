efb19c49f7ccad0a90a388e45c6ed301adaa3489
Fasil <fasil17062002@gmail.com>
2022-01-20 13:34:42 +0530

    onboard created

---
+++ lib/Screens/join_page.dart
@@ -0,0 +1,255 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class JoinPage extends StatelessWidget {
+  JoinPage({Key? key}) : super(key: key);
+  final fullnameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final _formkey = GlobalKey<FormState>();
+  bool isVisible = true;
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    final textFactor = MediaQuery.textScaleFactorOf(context);
+    if (MediaQuery.of(context).viewInsets.bottom > 20) {
+      isVisible = false;
+    } else {
+      isVisible = true;
+    }
+    return Scaffold(
+      backgroundColor: const Color(0xffFCFCFC),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        toolbarHeight: 80,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            size: 25,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Let's Starts here",
+                    style: GoogleFonts.poppins(
+                      decoration: TextDecoration.none,
+                      fontSize: textFactor * 28,
+                      color: Colors.black.withOpacity(0.8),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: height * .005,
+                  ),
+                  Text(
+                    "Fill in Your detial to begin",
+                    style: GoogleFonts.poppins(
+                      decoration: TextDecoration.none,
+                      fontSize: textFactor * 14,
+                      color: Colors.grey[600],
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  SizedBox(
+                    height: height * .025,
+                  ),
+                  TextFormField(
+                    controller: fullnameController,
+                    obscureText: true,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      filled: true,
+                      fillColor: const Color(0xffE7EAF0),
+                      contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                      hintText: "Fullname",
+                    ),
+                    validator: (value) {
+                      final regex = RegExp(r'^.{4,}$');
+                      if (value!.isEmpty) {
+                        return "Password is required for login";
+                      }
+                      if (!regex.hasMatch(value)) {
+                        return "Enter Valid Password(Min. 6 Character)";
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: height * .025,
+                  ),
+                  TextFormField(
+                    controller: emailController,
+                    obscureText: true,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      filled: true,
+                      fillColor: const Color(0xffE7EAF0),
+                      contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                      hintText: "Email",
+                    ),
+                    validator: (value) {
+                      final regex = RegExp(r'^.{4,}$');
+                      if (value!.isEmpty) {
+                        return "Password is required for login";
+                      }
+                      if (!regex.hasMatch(value)) {
+                        return "Enter Valid Password(Min. 6 Character)";
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: height * .025,
+                  ),
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: true,
+                    textInputAction: TextInputAction.done,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      filled: true,
+                      fillColor: const Color(0xffE7EAF0),
+                      contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                      hintText: "Password",
+                    ),
+                    validator: (value) {
+                      final regex = RegExp(r'^.{4,}$');
+                      if (value!.isEmpty) {
+                        return "Password is required for login";
+                      }
+                      if (!regex.hasMatch(value)) {
+                        return "Enter Valid Password(Min. 6 Character)";
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: height * .025,
+                  ),
+                  GestureDetector(
+                    onTap: () async {
+                      SystemChannels.textInput.invokeMethod("TextInput.hide");
+
+                      if (_formkey.currentState!.validate()) {
+                        await Future.delayed(const Duration(seconds: 2));
+                      }
+                    },
+                    child: Container(
+                      width: width,
+                      height: height * 0.065,
+                      decoration: BoxDecoration(
+                        gradient: const LinearGradient(
+                          colors: [
+                            Color(0xffFB724C),
+                            Color(0xffFE904B),
+                          ],
+                        ),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Sign up",
+                          style: GoogleFonts.poppins(
+                            decoration: TextDecoration.none,
+                            fontSize: textFactor * 14,
+                            color: Colors.white.withOpacity(0.9),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: height * 0.05),
+                child: AnimatedOpacity(
+                  opacity: isVisible ? 1 : 0,
+                  duration: const Duration(milliseconds: 200),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "By signing in, i agree with ",
+                            style: GoogleFonts.poppins(
+                              decoration: TextDecoration.none,
+                              fontSize: textFactor * 11,
+                              color: Colors.grey[600],
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                          Text(
+                            "Terms of Use",
+                            style: GoogleFonts.poppins(
+                              decoration: TextDecoration.none,
+                              fontSize: textFactor * 11,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: height * .0025,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "and ",
+                            style: GoogleFonts.poppins(
+                              decoration: TextDecoration.none,
+                              fontSize: textFactor * 11,
+                              color: Colors.grey[600],
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                          Text(
+                            "Privacy Policy",
+                            style: GoogleFonts.poppins(
+                              decoration: TextDecoration.none,
+                              fontSize: textFactor * 11,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/Screens/onboard_page.dart
+++ lib/Screens/onboard_page.dart
@@ -1,4 +1,7 @@
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:machine_test/Screens/join_page.dart';
 
 class OnBoardPage extends StatelessWidget {
   const OnBoardPage({Key? key}) : super(key: key);
@@ -8,16 +11,136 @@ class OnBoardPage extends StatelessWidget {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
     final textFactor = MediaQuery.textScaleFactorOf(context);
-    return Stack(
-      children: [
-        SizedBox(
-          height: height * .8,
-          child: Image.asset(
-            "assets/ONBOARDING PICTURE.png",
-            fit: BoxFit.cover,
-          ),
+    return Container(
+      height: height,
+      width: width,
+      color: const Color(0xff202020),
+      child: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              top: -30,
+              child: SizedBox(
+                height: height * .85,
+                child: Image.asset(
+                  "assets/ONBOARDING PICTURE.png",
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Positioned(
+              left: width * 0.03,
+              top: height * 0.055,
+              child: Image.asset(
+                "assets/icn1.png",
+              ),
+            ),
+            Positioned(
+              left: width * 0.14,
+              top: height * 0.062,
+              child: Image.asset(
+                "assets/dog.png",
+                height: height * .038,
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              child: SizedBox(
+                width: width,
+                child: Column(
+                  children: [
+                    Text(
+                      "Too tired to walk your dog?",
+                      style: GoogleFonts.poppins(
+                        decoration: TextDecoration.none,
+                        fontSize: textFactor * 19,
+                        color: Colors.white.withOpacity(0.9),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "Let’s help you!",
+                      style: GoogleFonts.poppins(
+                        decoration: TextDecoration.none,
+                        fontSize: textFactor * 19,
+                        color: Colors.white.withOpacity(0.9),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: height * .025,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => JoinPage(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        width: width,
+                        height: height * 0.065,
+                        decoration: BoxDecoration(
+                          gradient: const LinearGradient(
+                            colors: [
+                              Color(0xffFB724C),
+                              Color(0xffFE904B),
+                            ],
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        margin: EdgeInsets.symmetric(horizontal: width * 0.07),
+                        child: Center(
+                          child: Text(
+                            "Join our community",
+                            style: GoogleFonts.poppins(
+                              decoration: TextDecoration.none,
+                              fontSize: textFactor * 14,
+                              color: Colors.white.withOpacity(0.9),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: height * .025,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Alredy a member? ",
+                          style: GoogleFonts.poppins(
+                            decoration: TextDecoration.none,
+                            fontSize: textFactor * 12,
+                            color: Colors.white.withOpacity(0.9),
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                        Text(
+                          "Sign in",
+                          style: GoogleFonts.poppins(
+                            decoration: TextDecoration.none,
+                            fontSize: textFactor * 11,
+                            color: const Color(0xffFB724C),
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: height * .025,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
